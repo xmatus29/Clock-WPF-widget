52f339d97e48f44bafb2168f2c8e321070eca1bb
xmatus29 <xmatus29@stud.fit.vutbr.cz>
2015-10-29 15:39:36 +0100

    design ...

--- hours/hours/MainWindow.xaml.cs
+++ hours/hours/MainWindow.xaml.cs
@@ -35,24 +35,31 @@ namespace hours
             this.Top = 0;
             Info informacie = new Info();
 
+            /* treba pozdeji zmenit :) pocasi se nacte az pozdeji po spusteni */
+            while (informacie.teplota == null) {; }
+           
+
             /*
-             Pouzito z INFO ...  nedarilo se mi k te tride dostat ...
+             Casovac
             */
             this.timerCas = new System.Windows.Threading.DispatcherTimer();
             this.timerCas.Tick += new EventHandler(nastavCas);
             this.timerCas.Interval = new TimeSpan(0, 0, 1); //1 sekunda
             this.timerCas.Start();
+
+            /* pokus */
             okno.Width = okno.Height = 400;
             kolecko.Width = okno.Width - 50;
             kolecko.Height = okno.Height - 50;
             sekundaRucicka.Height = (kolecko.Height/2) - (kolecko.Height/16);
             minutaRucicka.Height = (kolecko.Height/2) - (kolecko.Height/8);
             hodinaRucicka.Height = (kolecko.Height/2) - (kolecko.Height/4);
-            
-            //System.Console.WriteLine(sekundaRucicka.Margin);
-            //minutaRucicka.Height = kolecko.Height - 40;
-            //hodinaRucicka.Height = kolecko.Height - 60;
 
+            /* sekce pocasi */
+            teplota.Content = informacie.teplota + " °C";
+            lokace.Content = informacie.lokacia;
+            System.Console.WriteLine(informacie.obrazokURL);
+            //obrazekPocasi.Source = http://www.icons.wxug.com/i/c/k/cloudy.gif;
         }
 
         private void nastavCas(object sender, EventArgs e)
@@ -63,8 +70,8 @@ namespace hours
 
             digitalTime.Content = DateTime.Now.Hour.ToString() + "h " + DateTime.Now.Minute.ToString() + "m " + DateTime.Now.Second.ToString() + "s";
             datum.Content = DateTime.Now.DayOfWeek + "  " + DateTime.Now.Day + ". " + DateTime.Now.Month + ". " + DateTime.Now.Year;
-            //Console.WriteLine(this.sekunda);
         }
+
 
         /*
          * Metoda, ktora sa stara aby sa dalo drag and drop pohybovat okno
