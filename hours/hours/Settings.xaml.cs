689ad419a6aaead0f0e9d8a5d5ae2b0724215ebc
Raiper34 <raipergm34@gmail.com>
2015-11-06 00:13:07 +0100

    este nove subory zase to tam nedalo...

---
+++ hours/hours/Settings.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace hours
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Settings : Window
+    {
+        public Settings()
+        {
+            InitializeComponent();
+            string[] skiny = Directory.GetDirectories(System.Environment.CurrentDirectory + "\\Skins");
+
+            foreach (string skin in skiny)
+            {
+                SkinBox.Items.Add(System.IO.Path.GetFileName(skin));
+                //nahlad.Source = new BitmapImage(new Uri(skin + "\\thumb.png"));
+            }
+        }
+
+        private void vyber(object sender, SelectionChangedEventArgs e)
+        {
+            string skin = (sender as ComboBox).SelectedItem as string;
+            nahlad.Source = new BitmapImage(new Uri(System.Environment.CurrentDirectory + "\\Skins\\" + skin + "\\thumb.png"));
+        }
+    }
+}
