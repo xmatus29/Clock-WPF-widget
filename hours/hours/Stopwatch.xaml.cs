5e53b209944434bc90a2c8bf6f9bb257933c7ba5
Raiper34 <raipergm34@gmail.com>
2015-12-11 23:13:52 +0100

    stopky funkcne

--- hours/hours/Stopwatch.xaml.cs
+++ hours/hours/Stopwatch.xaml.cs
@@ -19,9 +19,47 @@ namespace hours
     /// </summary>
     public partial class Stopwatch : Window
     {
+        public System.Diagnostics.Stopwatch cas;
+        private System.Windows.Threading.DispatcherTimer timer;
+
         public Stopwatch()
         {
             InitializeComponent();
+            this.cas = new System.Diagnostics.Stopwatch();
+            this.timer = new System.Windows.Threading.DispatcherTimer();
+            this.timer.Tick += new EventHandler(count);
+            this.timer.Interval = new TimeSpan(0, 0, 0, 0, 1); //1 hodina
+            this.timer.Start();
+        }
+
+        private void StartStopkyButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(this.cas.IsRunning)
+            {
+                this.cas.Stop();
+                StartStopkyButton.Background = Brushes.Green;
+                StartStopkyButton.Content = "START";
+            }
+            else
+            {
+                this.cas.Start();
+                StartStopkyButton.Background = Brushes.Red;
+                StartStopkyButton.Content = "STOP";
+            }
+        }
+
+        private void ResetStopkyButton_Click(object sender, RoutedEventArgs e)
+        {
+            StopkyZobrazovanie.Text = "00:00:00";
+            this.cas.Reset();
+        }
+
+        private void count(object sender, EventArgs e)
+        {
+            if(this.cas.IsRunning)
+            {
+                StopkyZobrazovanie.Text = this.cas.Elapsed.Minutes + ":" + this.cas.Elapsed.Seconds + ":"  + this.cas.Elapsed.Milliseconds;
+            }
         }
     }
 }
