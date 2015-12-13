36a5dab152f31681e80a0ee0d823119eff3b8e9f
fjezovica <fjezovica@gmail.com>
2015-12-13 19:08:44 +0100

    ythfgyjughjgj

--- hours/hours/Settings.xaml.cs
+++ hours/hours/Settings.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+
+
 namespace hours
 {
     /// <summary>
@@ -28,7 +30,8 @@ namespace hours
 
         public Settings()
         {
-           
+
+
             InitializeComponent();
             string[] skiny = Directory.GetDirectories(System.Environment.CurrentDirectory + "\\Skins");
 
